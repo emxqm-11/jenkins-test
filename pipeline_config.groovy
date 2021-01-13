9936ee5b17f257eb891c338e92e037107d24b170
Emily Ha <emily.ha@contino.io>
2021-01-13 12:14:05 +1100

    package.groovy ref

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,4 +1,20 @@
-libraries{
+jte{
+    pipeline_template="package"
+}
+libraries {
     maven
-    splunk
+    nexus
+    security
+    sonarqube
+}
+
+application_environments{
+    sit{
+        long_name = "System Integration Testing"
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    uat{
+        long_name = "User Acceptance Testing"
+        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+    }
 }
